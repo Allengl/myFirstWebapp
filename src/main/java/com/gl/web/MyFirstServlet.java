195f05d1507bb58fe85a965d581cc1202e76724d
guolong <guolong.deng@hand-china.com>
2023-09-07 15:39:52 +0800

    优化 Servlet 方法

--- src/main/java/com/gl/web/MyFirstServlet.java
+++ src/main/java/com/gl/web/MyFirstServlet.java
@@ -4,17 +4,11 @@ import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class MyFirstServlet implements Servlet {
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-
-    }
-
+public class MyFirstServlet extends MyGenericServlet {
     @Override
     public ServletConfig getServletConfig() {
         return null;
     }
-
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         PrintWriter pw = servletResponse.getWriter();
@@ -24,15 +18,5 @@ public class MyFirstServlet implements Servlet {
         pw.println("</body>");
         pw.println("</html>");
         pw.close();
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-
     }
 }
